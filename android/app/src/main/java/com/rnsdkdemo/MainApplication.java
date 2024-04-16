63634a9bd6b5a8f5c4d9c35faa82a77318bb16f7
wanghualei <wanghualei@finogeeks.com>
2024-04-16 17:23:44 +0800

    update 2.0.6 rn-sdk

--- android/app/src/main/java/com/rnsdkdemo/MainApplication.java
+++ android/app/src/main/java/com/rnsdkdemo/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.finogeeks.mop.rnsdk.FINMopSDKPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.zoontek.rnpermissions.RNPermissionsPackage;
 import org.reactnative.camera.RNCameraPackage;
